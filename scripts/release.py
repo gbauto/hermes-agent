a980ff23cc9f09a9fe73226a24342f744952e31e
gblack686 <gblack686@gmail.com>
2026-06-22 22:08:08 -0700

    Map Greg author email

--- scripts/release.py
+++ scripts/release.py
@@ -45,6 +45,7 @@ ACP_REGISTRY_MANIFEST = REPO_ROOT / "acp_registry" / "agent.json"
 
 # Auto-extracted from noreply emails + manual overrides
 AUTHOR_MAP = {
+    "gblack686@gmail.com": "gblack686",
     # teknium (multiple emails)
     "teknium1@gmail.com": "teknium1",
     "me@promplate.dev": "CNSeniorious000",
